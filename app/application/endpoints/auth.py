f073c22e8560a9c967c6f212e8e16323c0b25285
spiceworm <me@spiceworm.xyz>
2024-08-28 15:21:01 -0600

    Refactor auth functions into util/auth.py
    Need to access these function for all authenticated
    endpoints

--- app/application/endpoints/auth.py
+++ app/application/endpoints/auth.py
@@ -1,8 +1,4 @@
-from datetime import (
-    datetime,
-    timedelta,
-    timezone,
-)
+from datetime import timedelta
 from typing import Annotated
 
 from fastapi import (
@@ -11,83 +7,21 @@ from fastapi import (
     Request,
     status,
 )
-from fastapi.security import (
-    OAuth2PasswordBearer,
-    OAuth2PasswordRequestForm,
-)
-import jwt
-from jwt.exceptions import InvalidTokenError
+from fastapi.security import OAuth2PasswordRequestForm
 import pydantic
-import sqlalchemy as sa
 
 from . import auth_router
-from ..config import settings
 from ..models import UserModel
-
-
-HASHING_ALGORITHM = "HS256"
-
-
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/token")
+from ..util.auth import (
+    authenticate_user,
+    create_access_token,
+    get_current_active_user,
+)
 
 
 class Token(pydantic.BaseModel):
     access_token: str
     token_type: str
-
-
-async def get_user(db_session, username: str) -> UserModel | None:
-    async with db_session() as session:
-        async with session.begin():
-            q = sa.select(UserModel).filter_by(username=username)
-            results = await session.execute(q)
-            return results.scalars().one_or_none()
-
-
-async def authenticate_user(db_session, username: str, password: str) -> UserModel | None:
-    async with db_session() as session:
-        async with session.begin():
-            q = sa.select(UserModel).filter_by(
-                password=password,
-                username=username,
-                enabled=True,
-            )
-            results = await session.execute(q)
-            return results.scalars().one_or_none()
-
-
-def create_access_token(data: dict, expires_delta: timedelta):
-    to_encode = data.copy()
-    expire = datetime.now(timezone.utc) + expires_delta
-    to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, settings.secret_key, algorithm=HASHING_ALGORITHM)
-    return encoded_jwt
-
-
-async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)], request: Request):
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
-    try:
-        payload = jwt.decode(token, settings.secret_key, algorithms=[HASHING_ALGORITHM])
-        username = payload.get("sub")
-        if username is None:
-            raise credentials_exception
-    except InvalidTokenError:
-        raise credentials_exception
-    else:
-        user = await get_user(request.app.db_session, username)
-        if user is None:
-            raise credentials_exception
-        return user
-
-
-async def get_current_active_user(current_user: Annotated[UserModel, Depends(get_current_user)]):
-    if not current_user.enabled:
-        raise HTTPException(status_code=400, detail="Inactive user")
-    return current_user
 
 
 @auth_router.post("/token")
---
+++ app/application/util/auth.py
@@ -0,0 +1,80 @@
+from datetime import (
+    datetime,
+    timedelta,
+    timezone,
+)
+from typing import Annotated
+
+from fastapi import (
+    Depends,
+    HTTPException,
+    Request,
+    status,
+)
+from fastapi.security import OAuth2PasswordBearer
+import jwt
+from jwt.exceptions import InvalidTokenError
+import sqlalchemy as sa
+
+from ..config import settings
+from ..models import UserModel
+
+
+HASHING_ALGORITHM = "HS256"
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/token")
+
+
+async def authenticate_user(db_session, username: str, password: str) -> UserModel | None:
+    async with db_session() as session:
+        async with session.begin():
+            q = sa.select(UserModel).filter_by(
+                password=password,
+                username=username,
+                enabled=True,
+            )
+            results = await session.execute(q)
+            return results.scalars().one_or_none()
+
+
+def create_access_token(data: dict, expires_delta: timedelta):
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + expires_delta
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, settings.secret_key, algorithm=HASHING_ALGORITHM)
+    return encoded_jwt
+
+
+async def get_user(db_session, username: str) -> UserModel | None:
+    async with db_session() as session:
+        async with session.begin():
+            q = sa.select(UserModel).filter_by(username=username)
+            results = await session.execute(q)
+            return results.scalars().one_or_none()
+
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)], request: Request):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, settings.secret_key, algorithms=[HASHING_ALGORITHM])
+        username = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+    except InvalidTokenError:
+        raise credentials_exception
+    else:
+        user = await get_user(request.app.db_session, username)
+        if user is None:
+            raise credentials_exception
+        return user
+
+
+async def get_current_active_user(current_user: Annotated[UserModel, Depends(get_current_user)]):
+    if not current_user.enabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
