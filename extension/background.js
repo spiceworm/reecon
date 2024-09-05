ff49667cce7c6132f34d0c15a4147329db401089
spiceworm <me@spiceworm.xyz>
2024-09-04 20:30:26 -0600

    Set extension default settings on install only

--- extension/background.js
+++ extension/background.js
@@ -1,12 +1,15 @@
-browser.runtime.onInstalled.addListener(() => {
-    browser.storage.local.set({
-        accessToken: null,
-        baseUrl: 'https://reecon.xyz',
-        enableThreadProcessing: false,
-        enableUserProcessing: false,
-        hideBadJujuThreads: false,
-        minThreadSentiment: 0.05,
-        minUserAge: 0,
-        minUserIQ: 0
-    });
+browser.runtime.onInstalled.addListener((reasonObj) => {
+    if (reasonObj.reason === browser.runtime.OnInstalledReason.INSTALL) {
+        // Set default settings. Do not reset settings back to defaults for extension updates or browser updates.
+        browser.storage.local.set({
+            accessToken: null,
+            baseUrl: 'https://reecon.xyz',
+            enableThreadProcessing: false,
+            enableUserProcessing: false,
+            hideBadJujuThreads: false,
+            minThreadSentiment: 0.05,
+            minUserAge: 0,
+            minUserIQ: 10
+        });
+    }
 });
