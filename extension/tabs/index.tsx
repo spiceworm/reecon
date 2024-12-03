dcd63ebf79ec9178cffa943e2073a2ffff48b9e5
spiceworm <me@spiceworm.xyz>
2024-12-03 11:58:51 -0700

    Restrict access to context-query and content-filter tabs
    The user must enter a valid openai key for them to
    be able to access the context-query feature or for
    them to change content-filters. Also make /settings
    the default route.

--- extension/tabs/index.tsx
+++ extension/tabs/index.tsx
@@ -2,26 +2,29 @@ import "bootstrap/dist/css/bootstrap.min.css"
 
 import { MemoryRouter, Navigate, Route, Routes } from "react-router-dom"
 
+import { useStorage } from "@plasmohq/storage/dist/hook"
+
 import { ContentFilters } from "~tabs/views/contentFilters"
 import { ContextQuery } from "~tabs/views/contextQuery"
 import { Settings } from "~tabs/views/settings"
 import { RequireAuthentication } from "~util/components/authentication"
+import * as constants from "~util/constants"
+import * as storage from "~util/storage"
+import type * as types from "~util/types"
 import { Login } from "~util/views/auth/login"
 import { Signup } from "~util/views/auth/signup"
 
 export default function OptionsPage() {
+    const [producerSettings] = useStorage({ instance: storage.localStorage, key: constants.PRODUCER_SETTINGS }, (v: types.ProducerSettings) =>
+        v === undefined ? constants.defaultProducerSettings : v
+    )
+
+    const producerApiKeyMissing = producerSettings.openai.api_key.length === 0
+
     return (
         <MemoryRouter>
             <Routes>
-                <Route path={"/"} element={<Navigate to={"/content-filters"} replace={true} />} />
-                <Route
-                    path="/context-query"
-                    element={
-                        <RequireAuthentication>
-                            <ContextQuery />
-                        </RequireAuthentication>
-                    }
-                />
+                <Route path={"/"} element={<Navigate to={"/settings"} replace={true} />} />
                 <Route
                     path="/settings"
                     element={
@@ -30,14 +33,27 @@ export default function OptionsPage() {
                         </RequireAuthentication>
                     }
                 />
-                <Route
-                    path="/content-filters"
-                    element={
-                        <RequireAuthentication>
-                            <ContentFilters />
-                        </RequireAuthentication>
-                    }
-                />
+
+                {producerApiKeyMissing ? null : (
+                    <>
+                        <Route
+                            path="/context-query"
+                            element={
+                                <RequireAuthentication>
+                                    <ContextQuery />
+                                </RequireAuthentication>
+                            }
+                        />
+                        <Route
+                            path="/content-filters"
+                            element={
+                                <RequireAuthentication>
+                                    <ContentFilters />
+                                </RequireAuthentication>
+                            }
+                        />
+                    </>
+                )}
 
                 <Route path="/auth/login" element={<Login onSuccessRedirectPath={"/content-filters"} />} />
                 <Route path="/auth/signup" element={<Signup onSuccessRedirectPath={"/content-filters"} />} />
