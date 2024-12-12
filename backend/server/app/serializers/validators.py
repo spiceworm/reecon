b422f598d7b5e176b3cc328ec89718eb153bae48
spiceworm <me@spiceworm.xyz>
2024-12-11 17:58:31 -0700

    Do not allow OpenAI downtime to cause reecon downtime
    If the OpenAI API is throwing errors, do not allow their
    downtime to cause downtime in reecon. Assume the provided
    api key is valid so that the reecon API can still serve
    processed data.
    
    We can assume the api key is valid because when the
    user initially enters it in the extension, it will
    not be stored unless it passes a validation test.

--- backend/server/app/serializers/validators.py
+++ backend/server/app/serializers/validators.py
@@ -6,8 +6,14 @@ __all__ = ("validate_openai_api_key",)
 
 
 def validate_openai_api_key(producer_settings: dict):
-    client = openai.OpenAI(api_key=producer_settings["api_key"])
+    client = openai.OpenAI(api_key=producer_settings["api_key"], timeout=5)
     try:
         client.models.list()
     except openai.AuthenticationError:
         raise serializers.ValidationError("OpenAI API key is invalid.")
+    except openai.APIStatusError:
+        # If the OpenAI API is throwing errors, do not allow their downtime to cause downtime in reecon.
+        # Assume the provided api key is valid so that the reecon API can still serve processed data.
+        # We can assume the api key is valid because when the user initially enters it in the extension,
+        # it will not be stored unless it passes a validation test.
+        pass
