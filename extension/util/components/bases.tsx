69898821ceb2ca506c71c74e79bfc93a08b8861f
spiceworm <me@spiceworm.xyz>
2025-01-06 15:15:57 -0700

    Update dependencies
    There is currently a bug that prevents the extension
    from building without error. I created a new plasmo
    extension, installed all the same dependencies, and
    updated the versions in this repo to the versions
    used in the fresh plasmo extension. The build problem
    is still present, but at least all the dependencies
    are consistent now.

--- extension/util/components/bases.tsx
+++
@@ -1,9 +0,0 @@
-import { Container } from "reactstrap"
-
-export const Unauthenticated = ({ children }) => {
-    return (
-        <Container className={"p-3"} fluid={true}>
-            {children}
-        </Container>
-    )
-}
