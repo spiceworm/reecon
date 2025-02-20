cf300b2a6bc518f24c9e5f3fe56e0e9e472a575a
spiceworm <me@spiceworm.xyz>
2025-02-20 13:34:40 -0700

    Add main content script
    This content script contains mutation observers that
    watch for new rows being added to the DOM in the
    case when more rows for threads are loaded and new
    comments being loaded. If either happens, the content
    script will execute and process the threads and usernames
    on the page.

---
+++ extension/contents/index.ts
@@ -0,0 +1,48 @@
+import type { PlasmoCSConfig } from "plasmo"
+
+import * as contents from "~util/contents"
+
+export const config: PlasmoCSConfig = {
+    matches: ["https://*.reddit.com/", "https://*.reddit.com/r/*", "https://*.reddit.com/user/*"]
+}
+
+const rowObserver = new MutationObserver(async (mutationRecords) => {
+    for (const mutationRecord of mutationRecords) {
+        if ((mutationRecord.target as HTMLDivElement).id === "siteTable") {
+            await contents.execute()
+            break
+        }
+    }
+})
+
+const commentObserver = new MutationObserver(async (mutationRecords) => {
+    for (const mutationRecord of mutationRecords) {
+        if ((mutationRecord.target as HTMLDivElement).id.startsWith("siteTable")) {
+            await contents.execute()
+            break
+        }
+    }
+})
+
+if (document.readyState !== "loading") {
+    // run content script the first time the page loads. Subsequent executions are handled by `MutationObserver`s
+    contents.execute().then()
+
+    // run content script every 30 seconds to update the DOM with newly processed results
+    setInterval(contents.execute, 30_000)
+
+    const observerOptions = {
+        subtree: true,
+        childList: true
+    }
+
+    const rowObserverTarget = document.getElementById("siteTable")
+    if (rowObserverTarget !== null) {
+        rowObserver.observe(rowObserverTarget, observerOptions)
+    }
+
+    const commentObserverTarget = document.querySelector("div[id^=siteTable_]")
+    if (commentObserverTarget !== null && commentObserverTarget.classList.contains("nestedlisting")) {
+        commentObserver.observe(commentObserverTarget, observerOptions)
+    }
+}
