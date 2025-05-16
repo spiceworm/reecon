da60c81e3b240fa3cabbb28a942c22dc790c0c95
spiceworm <me@spiceworm.xyz>
2025-05-16 12:28:24 -0600

    Rebuild compose services prior to running tests
    Ensure all uv environments have up-to-date packages
    that include any changes made to the reecon package.

--- backend/run_tests.sh
+++ backend/run_tests.sh
@@ -5,4 +5,9 @@ set -e
 export TOX_ARGS="$*"
 
 docker compose -f docker-compose-test.yml down --remove-orphans
+
+# Rebuild all services so all uv environments have up-to-date
+# packages that include any changes made to the reecon package.
+docker compose build
+
 exec docker compose -f docker-compose-test.yml up --build
